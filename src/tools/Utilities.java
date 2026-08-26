5216eedd12a16a595cd72a276dad501fbafccf07
Yogu <yourmamagx@gmail.com>
2026-08-25 21:36:33 -0400

    added reading methods

---
+++ src/tools/Utilities.java
@@ -0,0 +1,19 @@
+package tools;
+
+import umamusume.*;
+
+import java.util.HashMap;
+
+public class Utilities {
+    public static HashMap<String, BaseUma> readBaseUmas() {
+        HashMap<String, BaseUma> umas = new HashMap<>();
+        // Reads "base umamusumes" and stores them in a hashmap.
+        return umas;
+    }
+
+    public static HashMap<String, Uma> readUmas() {
+        HashMap<String, Uma> umas = new HashMap<>();
+        // Reads "umas" and stores them in a hashmap.
+        return umas;
+    }
+}
